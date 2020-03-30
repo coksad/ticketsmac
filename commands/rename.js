0c344b436776c5ba515b894685cf3246bcc58e3b
coksad <62913612+coksad@users.noreply.github.com>
2020-03-30 23:56:01 +0100

    Create rename.js

---
+++ commands/rename.js
@@ -0,0 +1,31 @@
+const Discord = require("discord.js");
+const config = require('../config.json');
+module.exports = {
+  name: "rename",
+  description: "Renames a channel name to the name specified.",
+  usage: "[args]",
+  aliases: ["none"],
+  example: "rename",
+  args: false,
+  cooldown: config.cooldown,
+  guildOnly: true,
+  execute(message, args) {
+  
+  const client = message.client;
+    message.delete();
+
+        let channelName = args.slice(0).join(" ")
+      
+        if (args[0]){
+          message.channel.setName(channelName)
+          }
+    
+    const embed = new Discord.RichEmbed()
+      .setTitle("Channel Renamed")
+      .setColor(config.colour)
+      .setDescription( `I have renamed the channel name to your specified command.`)
+    message.channel.send(embed);
+
+        
+        if(!args[0]) return message.reply('Enter a name')
+  }};
