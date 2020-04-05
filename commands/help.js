8bc060ab8e24f2f5034731a9980eea9064e0871e
coksad <62913612+coksad@users.noreply.github.com>
2020-04-05 19:05:52 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -36,7 +36,7 @@ module.exports = {
 					.setDescription(`\nType \`${config.prefix}help [command]\` for more information about a specific command.`)
 	        // .addField("...", `...`, true)
 	        // .addField("...", `...`, true)
-	         .setFooter(`Perfumiez Support`);
+	         .setFooter(`Sweetology Support`);
 
 					var cmds = [];
 					cmds.push(commands.map(command => embed.addField(`${config.prefix}${command.name}`, `\`${command.description}\``)));
@@ -65,7 +65,7 @@ module.exports = {
 				const cmd = new Discord.RichEmbed()
 	        .setColor(config.colour)
 					.addField(`Command`,`\`${command.name}\``, true)
-	         .setFooter(`Perfumiez Support`)
+	         .setFooter(`Sweetology Support`)
 
 				if (command.aliases) cmd.addField("Aliases", `\`${command.aliases.join(', ')}\``, true);
 				if (command.description) cmd.addField("Description", `\`${command.description}\``, false);
